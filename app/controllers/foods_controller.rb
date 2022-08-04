e37885cf9c6487f6f4b723099dcab22c9b44edaf
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2022-08-04 02:25:17 +0200

    solve the issue of controller error

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,33 @@
-class FoodController < ActionController::Base
-  def index; end
+class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
 
-  def show; end
+  def new
+    redirect_to foods_path, flash: { alert: 'Please sign up or login!' } unless current_user
 
-  def new; end
+    @food = Food.new
+  end
+
+  def create
+    @new_food = current_user.foods.new(food_params)
+    if @new_food.save
+      redirect_to foods_path, flash: { alert: 'Your food is saved' }
+    else
+      redirect_to new_food_path, flash: { alert: 'Could not save your food' }
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy!
+    flash[:notice] = 'You have deleted the food!'
+    redirect_to foods_path
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
 end
