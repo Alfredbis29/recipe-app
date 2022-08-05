e70b5abab3a52088b4ee24113332a727facf47e7
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-08-05 19:28:11 +0200

    display reciple

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,33 +5,45 @@ class RecipesController < ActionController::Base
 
   def show; end
 
+  
+  # def create
+  #   @recipe = Recipe.new(recipe_params)
+  
+  #   respond_to do |format|
+  #     if @recipe.save
+  #       format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
+  #       format.json { render :show, status: :created, location: @recipe }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @recipe.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  
+  # def update
+  #   respond_to do |format|
+  #     if @recipe.update(recipe_params)
+  #       format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @recipe }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @recipe.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
   def new
     @recipe = Recipe.new
   end
-
   def create
+   
+
     @recipe = Recipe.new(recipe_params)
-
-    respond_to do |format|
-      if @recipe.save
-        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
-        format.json { render :show, status: :created, location: @recipe }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @recipe.update(recipe_params)
-        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully updated.' }
-        format.json { render :show, status: :ok, location: @recipe }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
-      end
+    @recipe.user_id = current_user.id
+  
+    if @recipe.save
+      redirect_to recipes_path, flash: { alert: 'Your Recipe has been saved' }
+    else
+      redirect_to new_recipe_path, flash: { alert: 'Could not save your recipe' }
     end
   end
 
@@ -43,7 +55,7 @@ class RecipesController < ActionController::Base
 
 
     def recipe_params
-      params.permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
+      params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :user_id)
     end
 
 end
