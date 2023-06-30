d0dc29e76b772adea8444f31277077c36656c59f
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-06-30 20:02:01 +0200

    fix bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :encrypted_password
+
+  def password=(password)
+    self.encrypted_password = encrypt_password(password)
+  end
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :foods
